cce0c04adf4154cd7c4d73cf401cfe8a565a224f
Luna <malalunapg@gmail.com>
2018-11-17 15:56:43 -0500

    - Added inventory view - Added menu to add item in inventory

---
+++ Assets/addButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class addButton : MonoBehaviour {
+
+    public Text item;
+    public Text amount;
+    private ElementalInventory inventory;
+
+    // Use this for initialization
+    public void addItem () {
+        if(inventory == null) {
+            inventory = FindObjectOfType(typeof(ElementalInventory)) as ElementalInventory;
+        }
+        inventory.addItem(item.text, int.Parse(amount.text), new Color(Random.value / 2f, Random.value / 2f, Random.value / 2f, 1f));
+	}
+}
---
+++ Assets/quantityDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class quantityDisplay : MonoBehaviour {
+
+    private Slider sliderAmount;
+    public Text textAmount;
+    // Use this for initialization
+    void Start () {
+        sliderAmount = GetComponent<Slider>();
+        sliderAmount.value = 1; 
+        textAmount.text = 1.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        textAmount.text = sliderAmount.value.ToString();
+	}
+}
