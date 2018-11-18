3e1cee640c0cfe077e86df720f1716913c480060
Luna <malalunapg@gmail.com>
2018-11-18 18:09:26 -0500

    reset create item fields

--- Assets/addButton.cs
+++ Assets/addButton.cs
@@ -5,22 +5,30 @@ using UnityEngine.UI;
 
 public class addButton : MonoBehaviour {
 
-    public Text item;
+    public InputField itemField;
     public Text amount;
+    public Slider slider;
     public GameObject menuPanel;
     private ElementalInventory inventory;
 
     // Use this for initialization
     public void addItem () {
+
         if(inventory == null) {
             inventory = FindObjectOfType(typeof(ElementalInventory)) as ElementalInventory;
         }
-        inventory.addItem(item.text, int.Parse(amount.text), new Color(Random.value / 2f, Random.value / 2f, Random.value / 2f, 1f));
+        inventory.addItem(itemField.text, int.Parse(amount.text), new Color(Random.value / 2f, Random.value / 2f, Random.value / 2f, 1f));
         menuPanel.SetActive(false);
 	}
 
     public void openMenu() {
+        this.clean();
         menuPanel.SetActive(true);
- 
+    }
+
+    private void clean() {
+        itemField.text = "";
+        amount.text = "1";
+        slider.value = 1;
     }
 }
