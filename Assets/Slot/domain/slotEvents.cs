694087142019dabe5abdf85bdd1d1cf2a9b72eff
Luna <malalunapg@gmail.com>
2020-05-04 19:52:26 -0400

    feat(slot): add modify and delete method, add subscription file

--- Assets/Slot/domain/slotEvents.cs
+++ Assets/Slot/domain/slotEvents.cs
@@ -15,18 +15,25 @@ public class slotEvents : MonoBehaviour
   public void Start(){
     this.sprite=GetComponent<Sprite> ( );
     this.slotModel=ScriptableObject.CreateInstance<slotModel>();
-    this.setData (slotModel.itemName, slotModel.quantity, slotModel.image);
+    this.addItem (slotModel.itemName, slotModel.quantity, slotModel.image);
   }
 
-  private void setData(string name, int quantity, Sprite sprite) {
+  private void addItem(string name, int quantity, Sprite sprite) {
     this.name.text=name;
     this.quantity.text=quantity.ToString();
     this.sprite=sprite;
   }
 
-  // Start is called before the first frame update
-  void addItem()
-    {
-        
-    }
+  private void modifyItem (string name, int quantity, Sprite sprite){
+    this.name.text=name;
+    this.quantity.text=quantity.ToString ( );
+    this.sprite=sprite;
+  }
+
+  private void deleteItem(string name, int quantity, Sprite sprite){
+    this.slotModel.name="empty";
+    this.slotModel.quantity=0;
+    this.slotModel.image=null;
+    this.addItem (slotModel.itemName, slotModel.quantity, slotModel.image);
+  }
 }
